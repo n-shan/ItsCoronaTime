9c9d9c2b934c7670921b3e52cccc3682d783bbeb
n-shan <47575072+n-shan@users.noreply.github.com>
2020-05-05 14:58:02 -0700

    Implement Entity interface

--- src/itscoronatime/Entity.java
+++ src/itscoronatime/Entity.java
@@ -1,4 +1,10 @@
 package itscoronatime;
 
 public interface Entity {
+    public boolean isDead();
+    public void setDead();
+
+    public int getXLocation();
+    public int getYLocation();
+    public void setLocation(int x, int y);
 }
