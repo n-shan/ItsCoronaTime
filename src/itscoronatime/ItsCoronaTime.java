07ab83df7b5f3919e53fc196b5267c0eb28c2810
greg-clark <gregclark03@gmail.com>
2020-05-05 15:04:10 -0700

    Added Corona Time Start Screen

--- src/itscoronatime/ItsCoronaTime.java
+++ src/itscoronatime/ItsCoronaTime.java
@@ -1,11 +1,18 @@
 package itscoronatime;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class ItsCoronaTime extends Application {
 
     public static void main(String[] args) { launch(args); }
+
+    //adds in all the different pictures
+    private Image image = new Image("image/StartScreen.png");
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -13,6 +20,17 @@ public class ItsCoronaTime extends Application {
         //starts the game
         //Arena arena = new Arena
 
-        //here is a test for itscorona time
+        Pane pane = new Pane();
+
+        pane.getChildren().add(new ImageView(image));
+
+        Scene scene = new Scene(pane, 2220, 1248);
+
+        primaryStage.setTitle("Its Corona Time");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
     }
 }
