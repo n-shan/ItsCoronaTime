55175da31a10de4b5f1c60471746c5e02e955f7f
greg-clark <gregclark03@gmail.com>
2020-05-07 20:46:36 -0700

    Added constructor, helper functions, and private variables to person class

--- src/itscoronatime/Person.java
+++ src/itscoronatime/Person.java
@@ -1,6 +1,42 @@
 package itscoronatime;
 
 public class Person extends Entity {
+    private int lives;
+    private int score;
+    private String imageName = "image/PacMan.png";
+    private int xPos, yPos;
 
+    public Person(int x, int y)
+    {
+        xPos = x;
+        yPos = y;
+        lives = 3;
+        score = 0;
+    }
+
+    public String getImageName()
+    {
+        return imageName;
+    }
+
+    public int getLives()
+    {
+        return lives;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public int getX()
+    {
+        return xPos;
+    }
+
+    public int getY()
+    {
+        return yPos;
+    }
 
 }
