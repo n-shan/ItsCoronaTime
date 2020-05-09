11e7ff589194ae091c819c7e7274698518d907db
luisledezma325 <luis.ledezma325@gmail.com>
2020-05-09 16:55:53 -0700

    Implemented toiletpaper and coronavirus class

--- src/itscoronatime/Coronavirus.java
+++ src/itscoronatime/Coronavirus.java
@@ -1,4 +1,31 @@
 package itscoronatime;
 
 public class Coronavirus extends Entity {
+
+    String image = "image/Ghost.png";
+    boolean canDie = false;
+
+    public Coronavirus(int x, int y){
+        setLocation(x,y);
+        setVisibility(true);
+    }
+
+    //when hazmat suit is collected, allow virus to be killed
+    public void scare(Person person){
+        canDie = true;
+    }
+
+
+    public boolean isScared(Person person){
+        return canDie;
+    }
+
+    public String getImageName(){
+        return image;
+    }
+
+    //hazmat suit is off, allow virus to kill person
+    public void kill(Person person){
+        canDie = false;
+    }
 }
--- src/itscoronatime/Toiletpaper.java
+++ src/itscoronatime/Toiletpaper.java
@@ -1,5 +1,23 @@
 package itscoronatime;
 
 public class Toiletpaper extends Entity {
-    private final int pointVal = 10;
+    String image = "image/Pellet.png";
+    boolean isCollected = false;
+
+    private int pointVal = 10;
+
+    public Toiletpaper(int x, int y){
+        setLocation(x,y);
+        setVisibility(true);
+    }
+
+    public void collect(Person person){
+        setVisibility(false);
+        isCollected = true;
+
+    }
+
+    public String getImage() {
+        return image;
+    }
 }
