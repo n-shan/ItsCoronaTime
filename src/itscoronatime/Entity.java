df1b885a38b5d0a494400e47914b0c842f6b080e
n-shan <47575072+n-shan@users.noreply.github.com>
2020-05-09 20:25:45 -0700

    FIx entity class, implement in person and hazmat suit

--- src/itscoronatime/Entity.java
+++ src/itscoronatime/Entity.java
@@ -1,7 +1,7 @@
 package itscoronatime;
 
 public abstract class Entity {
-    private int x,y;
+    private int x,y, width, height;
     private boolean isVisible;
 
     public boolean isVisible() {
@@ -11,14 +11,27 @@ public abstract class Entity {
         this.isVisible = isVisible;
     }
 
-    public int getXLocation() {
+    public int getX() {
         return x;
     }
-    public int getYLocation() {
+    public int getY() {
         return y;
     }
     public void setLocation(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
