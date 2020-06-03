6f0d981548de05cd0cb71aeffaa43ca9cd1e3e85
n-shan <47575072+n-shan@users.noreply.github.com>
2020-06-02 19:25:43 -0700

    Add boolean to describe if virus is in the cage

--- src/itscoronatime/Coronavirus.java
+++ src/itscoronatime/Coronavirus.java
@@ -2,14 +2,16 @@ package itscoronatime;
 
 public class Coronavirus extends Entity {
 
-    String image = "image/Ghost.png";
-    boolean canDie = false;
+    private String image = "image/Ghost.png";
+    private boolean canDie = false;
+    private boolean isInCage;
 
     public Coronavirus(int x, int y){
         setLocation(x,y);
         setWidth(50);
         setHeight(50);
         setVisibility(true);
+        isInCage = true;
     }
 
     //when hazmat suit is collected, allow virus to be killed
@@ -28,5 +30,13 @@ public class Coronavirus extends Entity {
     //hazmat suit is off, allow virus to kill person
     public void kill(){
         canDie = false;
+    }
+
+    public void setIsInCage(boolean isInCage) {
+        this.isInCage = isInCage;
+    }
+
+    public boolean getIsInCage() {
+        return isInCage;
     }
 }
